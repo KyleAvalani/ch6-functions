819ad0f605f0750f18538fb94da9faf1dc8e4247
Kyle <kyleavalani@msn.com>
2017-10-05 15:02:27 -0700

    Make variable names more descriptive

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,16 +1,16 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-AddThree <- function(a){
-  return(a + 3)
+AddThree <- function(num){
+  return(num + 3)
 }
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
 ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
-FeetToMeters <- function(a){
-  return(a * 0.3048)
+FeetToMeters <- function(feet){
+  return(feet * 0.3048)
 }
 
 # Create a variable `height.in.feet` that is your height in feet
